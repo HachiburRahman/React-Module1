b5b287877de5504247938e14d22bb44351470697
Hachibur Rahman <hachiburrahman15@gmail.com>
2026-04-18 14:30:12 +0600

    Basic JS

---
+++ javascript.js
@@ -0,0 +1,35 @@
+let name = "Hachibur Rahman";
+name = "Hachibur Rahman Antu ";
+// console.log(name);
+const friends = ["Avik", "Moon", "Jubayer", "Jalal"];
+// console.log(friends);
+for (let friend of friends) {
+  // console.log(friend);
+}
+
+const num=40;
+if(num>=10 || num<=30)
+{
+    // console.log("YES");
+}
+else{
+    // console.log("NOoooo");
+}
+
+function sum(a,b){
+    return a+b;
+}
+
+const result=sum(5,10);
+
+// console.log(result);
+
+const person={
+    name:"Antu",
+    age:25,
+    friends:friends
+}
+// console.log(person);
+for(let i in person){
+    // console.log(person[i]);
+}
